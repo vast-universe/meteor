e0e1304a101a275184c052263ee8240295bb6f85
huangpeng <55933410+vast-universe@users.noreply.github.com>
2024-08-03 23:10:58 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    reactCompiler: true, //react 19实验性支持
+    staleTimes: { //路由缓存时间，针对APP Router
+      dynamic: 30,
+      static: 180,
+    },
+  },
+};
 
 export default nextConfig;
